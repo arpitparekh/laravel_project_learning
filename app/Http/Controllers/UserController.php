688b86b9956802d840d31653402d5a2cdc7c5f88
arpitparekh <arpitparekh9@gmail.com>
2023-10-08 23:36:45 +0530

    validater update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,17 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;   // import for hashing a password
+use Validator;
 
 class UserController extends Controller
 {
     public function register(Request $request){   // register user function
         
-        $request->validate([              // this is for validation
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|confirmed',
-            'tc' => 'required',
+        $validator = Validator::make($request->all(), [    // this is for validation
+        'name' => 'required',
+        'email' => 'required|email',
+        'password' => 'required|min:6|confirmed',
+        'tc' => 'required',
         ]);
+
+        if ($validator->fails()) {
+            return response(['message' => $validator->errors()->first(), 'status' => 'failed'], 200);
+        }
 
         if(User::where('email',$request->input('email'))->first()){
             return response([
@@ -32,14 +37,14 @@ class UserController extends Controller
 
         // $user->save();
 
-        $user = User::create([                                  // use this insted of upper code
+        User::create([                                  // use this insted of upper code
             'name' => $request->input('name'),
             'email' => $request->input('email'),
             'password' => Hash::make($request->input('password')),       // hash password
             'tc' => json_decode($request->input('tc')),
         ]);
 
-        return response()->json([
+        return response([
             'message' => 'User Registered Succesfully',
             'status' => 'success'
         ], 201);
